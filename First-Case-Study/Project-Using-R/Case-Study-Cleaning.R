ed2cd63c8a1e9af27403f4bfe092f4dcde89fbe7
K.Bouskou <84814094+K-Bouskou@users.noreply.github.com>
2021-12-05 00:50:13 +0100

    Change Name

--- First-Case-Study/Project-Using-R/Case-Study-Cleaning.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-library(lubridate)
-
-annual_v2 <- read.csv("C:/Users/KasiolEr/Desktop/annual.csv")
-str(counts)
-annual_v2$ride_id <- as.numeric(annual_v2$ride)
-counts <- aggregate(annual_v2$rideable_type ~ annual_v2$member_casual + annual_v2$day_of_week, FUN = mean)
-write.csv(counts, "C:/Users/KasiolEr/Desktop/number_of_ridesl.csv")
